15ed00394b02d810b4a8f36630d14c05807a2663
9R2by <37716834+9R2by@users.noreply.github.com>
2025-07-01 03:34:07 +0200

    test

--- src/run.c
+++ src/run.c
@@ -26,8 +26,9 @@
 #include "argon2.h"
 #include "core.h"
 
-#define T_COST_DEF 3
-#define LOG_M_COST_DEF 12 /* 2^12 = 4 MiB */
+#define T_COST_DEF 2
+//#define LOG_M_COST_DEF 12 /* 2^12 = 4 MiB */
+#define LOG_M_COST_DEF 15 /* 2^15 = 32 MiB */
 #define LANES_DEF 1
 #define THREADS_DEF 1
 #define OUTLEN_DEF 32
